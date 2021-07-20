2c61fe517930e2e119e75d6f60f9a26b4e84ae8e
Maksim Karsakou1 <maksim_karsakou1@epam.com>
2021-07-20 16:11:42 +0300

    update decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,6 +16,9 @@ import {
   ApiNotFoundResponse,
   ApiInternalServerErrorResponse,
   ApiTags,
+  ApiParam,
+  ApiBody,
+  ApiQuery,
 } from '@nestjs/swagger';
 import { Response } from 'express';
 
@@ -23,7 +26,7 @@ import { UsersService } from './users.service';
 import { CreateUserDto, CreatedUserRes } from './dto/create-user.dto';
 import { AddUserToGroup } from './dto/add-to-group.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { methodExecutionTimeLogger } from '../utils/loggers/time-logget/timelogger';
+import { LogDecorator } from '../utils/loggers/time-logget/timelogger';
 
 @ApiTags('users')
 @Controller('users')
@@ -33,14 +36,13 @@ export class UsersController {
   @Post()
   @ApiCreatedResponse({ description: 'User created', type: CreatedUserRes })
   @ApiBadRequestResponse({ description: 'Validation error' })
+  @ApiBody({ type: [CreateUserDto] })
+  @LogDecorator()
   async create(
     @Body() createUserDto: CreateUserDto,
     @Res() response: Response,
   ) {
-    const userData = await methodExecutionTimeLogger(this.usersService.create)(
-      this.usersService,
-      createUserDto,
-    );
+    const userData = await this.usersService.create(createUserDto);
     return response.send(userData);
   }
 
@@ -50,46 +52,48 @@ export class UsersController {
     type: AddUserToGroup,
   })
   @ApiBadRequestResponse({ description: 'Validation error' })
+  @ApiBody({ type: [AddUserToGroup] })
+  @LogDecorator()
   async addUserToGroup(
     @Body() addUserToGroup: AddUserToGroup,
     @Res() response: Response,
   ) {
-    const resp = await methodExecutionTimeLogger(
-      this.usersService.addUserToGroup,
-    )(this.usersService, addUserToGroup);
+    const resp = await this.usersService.addUserToGroup(addUserToGroup);
     return response.send(resp);
   }
 
   @Get('suggested')
   @ApiOkResponse({ description: 'Users received' })
+  @ApiQuery({ name: 'loginSubstring', type: 'string' })
+  @ApiQuery({ name: 'limit', type: 'number' })
+  @LogDecorator()
   async getSuggested(
     @Query('loginSubstring') loginSubstring: string,
     @Query('limit') limit: number,
     @Res() response: Response,
   ) {
-    const userData = await methodExecutionTimeLogger(
-      this.usersService.getSuggested,
-    )(this.usersService, loginSubstring, limit);
+    const userData = await this.usersService.getSuggested(
+      loginSubstring,
+      limit,
+    );
     return response.send(userData);
   }
 
   @Get(':id')
   @ApiOkResponse({ description: 'User received' })
   @ApiBadRequestResponse({ description: 'Invalid id' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @LogDecorator()
   async findOne(@Param('id') id: string, @Res() response: Response) {
-    const userData = await methodExecutionTimeLogger(this.usersService.findOne)(
-      this.usersService,
-      id,
-    );
+    const userData = await this.usersService.findOne(id);
     return response.send(userData);
   }
 
   @Get()
   @ApiOkResponse({ description: 'All users received' })
+  @LogDecorator()
   async findAll(@Res() response: Response) {
-    const userData = await methodExecutionTimeLogger(this.usersService.findAll)(
-      this.usersService,
-    );
+    const userData = await this.usersService.findAll();
     return response.send(userData);
   }
 
@@ -98,27 +102,25 @@ export class UsersController {
   @ApiBadRequestResponse({ description: 'Validation error' })
   @ApiNotFoundResponse({ description: 'Invalid id' })
   @ApiInternalServerErrorResponse({ description: 'Update failed' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({ type: [UpdateUserDto] })
+  @LogDecorator()
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
     @Res() response: Response,
   ) {
-    const userData = await methodExecutionTimeLogger(this.usersService.update)(
-      this.usersService,
-      id,
-      updateUserDto,
-    );
+    const userData = await this.usersService.update(id, updateUserDto);
     return response.send(userData);
   }
 
   @Delete(':id')
   @ApiOkResponse({ description: 'User deleted' })
   @ApiNotFoundResponse({ description: 'Invalid id' })
+  @ApiParam({ name: 'id', type: 'string' })
+  @LogDecorator()
   async remove(@Param('id') id: string, @Res() response: Response) {
-    const userData = await methodExecutionTimeLogger(this.usersService.remove)(
-      this.usersService,
-      id,
-    );
+    const userData = await this.usersService.remove(id);
     return response.send(userData);
   }
 }
--- src/utils/loggers/time-logget/timelogger.ts
+++ src/utils/loggers/time-logget/timelogger.ts
@@ -4,20 +4,34 @@ import CustomLogger from '../common-logger/logger.servece';
 
 const logger = new CustomLogger('Time logger');
 
-export const methodExecutionTimeLogger = (func: any) => async (
-  context,
-  ...args
-) => {
-  let start;
-  try {
-    start = performance.now();
-    const result = await func.call(context, ...args);
-    const end = performance.now();
-    logger.log(`${func.name} take ${end - start} ms`);
-    return result;
-  } catch (error) {
-    const end = performance.now();
-    logger.log(`${func.name} take ${end - start} ms`);
-    return error;
-  }
-};
+export function LogDecorator() {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  let originalFunc: Function;
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    originalFunc = descriptor.value;
+    descriptor.value = async function (...args) {
+      let start;
+      try {
+        start = performance.now();
+        const result = await originalFunc.apply(this, args);
+        const end = performance.now();
+        logger.log(
+          `Log from decorator ${originalFunc.name} take ${end - start} ms`,
+        );
+        return result;
+      } catch (error) {
+        const end = performance.now();
+        logger.log(
+          `Log from decorator error ${originalFunc.name} take ${
+            end - start
+          } ms`,
+        );
+        return error;
+      }
+    };
+  };
+}
